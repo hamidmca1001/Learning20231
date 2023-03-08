c0c23c4476e5e7330e5fa86cc82e48ea399951fa
Hamid Ali <hamid.khan321@hotmail.com>
2023-03-08 18:08:38 +0530

    Add UTC basic

---
+++ Learning2023/xUnitTestExcrcise/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning2023.xUnitTestExcrcise
+{
+    public class Hero
+    {
+        private const int ExperienceNeeded = 1000;
+
+        public int Level { get; set; }
+        public int Experience { get; set; }
+
+        public Hero(int experience)
+        {
+            Level = 0;
+            Experience = experience;
+        }
+
+        public void LevelUp()
+        {
+            if (Experience - ExperienceNeeded < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(Experience),
+                    "Not enough Experience to level up!");
+            }
+
+            Experience -= ExperienceNeeded;
+            Level++;
+        }
+
+        public async Task LevelUpAsync()
+        {
+            if (Experience - ExperienceNeeded < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(Experience),
+                    "Not enough Experience to level up asynchronously!");
+            }
+
+            Experience -= ExperienceNeeded;
+            Level++;
+
+            await Task.CompletedTask;
+        }
+    }
+}
